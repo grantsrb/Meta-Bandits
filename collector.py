b20b8d7e1d1cd3c41ad5ad18668ea4666d8ad893
Satchel Grant <grantsrb@gmail.com>
2018-04-02 19:20:56 -0700

    remove bootstrapping

--- collector.py
+++ collector.py
@@ -2,6 +2,7 @@ import torch
 from torch.autograd import Variable
 import torch.nn.functional as F
 import numpy as np
+import bandit
 
 class Collector():
 
@@ -15,36 +16,49 @@ class Collector():
             t_obj = t_obj.cuda()
         return t_obj
 
-    def __init__(self, net, envs, n_tsteps):
+    def __init__(self, net, n_envs, n_bandits):
         self.net = net # PyTorch Module
-        self.envs = envs # Vector of bandit envs
-        self.n_tsteps = n_tsteps
-        self.pi_space = self.envs[0].n_bandits
-        self.softmax = envs[0].softmax
+        self.pi_space = n_bandits
+        self.n_envs = n_envs
+        self.softmax = bandit.Bandit().softmax
 
-    def rollout(self):
-        data = {'actions':[], 'rewards':[], 'values':[]}
+    def rollout(self, n_tsteps):
+        self.envs = self.new_envs() # Vector of bandit envs
+        data = {'actions':[], 'sparse_actions':[], 'rewards':[], 'values':[]}
         self.net.reset_state(len(self.envs))
         self.net.train(mode=False)
         net_input = Variable(self.cuda_if(torch.zeros(len(self.envs),self.pi_space+1)))
-        net_inputs = self.cuda_if(torch.zeros(self.n_tsteps,len(self.envs),self.pi_space+1))
-        for i in range(0, self.n_tsteps):
+        net_inputs = self.cuda_if(torch.zeros(n_tsteps,len(self.envs),self.pi_space+1))
+        for i in range(0, n_tsteps):
             net_inputs[i] = net_input.data
             outputs,vals = self.net.forward(net_input)
             pis = self.softmax(self.cpu_if(outputs.data).numpy())
             actions = self.get_actions(pis)
             rewards = self.get_rewards(actions)
+            data['sparse_actions'].append(np.argmax(actions, axis=-1))
             data['actions'].append(actions)
             data['rewards'].append(rewards)
-            data['values'].append(self.cpu_if(vals.data).numpy())
+            data['values'].append(self.cpu_if(vals.data.squeeze()).numpy())
             net_input = self.get_net_input(actions, rewards)
         
-        outputs,vals = self.net.forward(net_input)
-        data['values'].append(self.cpu_if(vals.data).numpy())
+        outputs, vals = self.net.forward(net_input)
+        data['values'].append(self.cpu_if(vals.data.squeeze()).numpy())
         for key in data.keys():
             data[key] = np.asarray(data[key], dtype=np.float32)
+        data['rewards'] = data['rewards'].squeeze()
         data['net_inputs'] = net_inputs
         return data
+
+    def new_envs(self):
+        """
+        Makes a new list of bandit environments.
+        """
+        envs = []
+        for i in range(self.n_envs):
+            rand = np.random.random()
+            probs = [.1,.9] if rand <= 0.5 else [.9,.1]
+            envs.append(bandit.Bandit(probs=probs))
+        return envs
 
     def get_actions(self, pis):
         """
@@ -61,7 +75,8 @@ class Collector():
             cumsums += pis[:,i]
             actions[:,i] = (randoms < cumsums)
             randoms += actions[:,i]
-        return actions.astype(np.float32)
+        actions = actions.astype(np.float32)
+        return actions
     
     def get_rewards(self, actions):
         """
