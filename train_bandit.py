4099c04d2c4df53386d2d0687e932b09da1db62f
Satchel Grant <grantsrb@gmail.com>
2018-04-02 19:23:35 -0700

    add more logging statements, move envs to collector, minor changes

--- train_bandit.py
+++ train_bandit.py
@@ -2,49 +2,48 @@ import torch
 from torch.autograd import Variable
 import numpy as np
 from model import Model
-import bandit
 from collector import Collector
 from updater import Updater
 
-
-
 n_envs = 100
-n_tsteps = 100
+n_tsteps = 50
 n_bandits = 2
 emb_size = 100
 total_steps = 100000000
-lr = 1e-3
+lr = 1e-4
 gamma = 0.99
 lambda_ = 0.96
-val_coef = .5
+val_coef = .1
 entr_coef = .01
 max_norm = .5
-
-# Make bandits
-envs = []
-for i in range(n_envs):
-    envs.append(bandit.Bandit(n_bandits))
+epochs_per_update = 1
+norm_advs = False
 
 # Make Model
 net = Model(n_bandits, emb_size, batch_size=n_envs)
-collector = Collector(net, envs, n_tsteps)
-updater = Updater(net,lr, gamma=gamma, lambda_=lambda_, val_coef=val_coef, entr_coef=entr_coef, max_norm=max_norm)
+collector = Collector(net, n_envs, n_bandits)
+updater = Updater(net,lr, gamma=gamma, lambda_=lambda_, val_coef=val_coef, entr_coef=entr_coef, max_norm=max_norm, norm_advs=norm_advs)
 
 T = 0
 avg_rew = 0
+epoch = 0
 while T < total_steps:    
     # Collect Rollout
-    print("\nStep:", T)
-    data = collector.rollout()
+    epoch += 1
+    print("\nStep:", T, " – Epoch", epoch)
+    data = collector.rollout(n_tsteps)
     T += n_tsteps*n_envs
-    avg_rew = np.mean(data['rewards'])
-    print("Avg Rew:", avg_rew)
-
     # Update Model
     updater.calc_loss(data)
-    updater.update_model(calc_grad=True)
+    if epoch % epochs_per_update == 0:
+        avg_rew = np.mean(data['rewards'])
+        avg_val = np.mean(data['values'])
+        avg_action = np.mean(data['sparse_actions'])
+        print("Avg Rew:", avg_rew, " – Avg Act:", avg_action, " – Avg Val:",avg_val)
+        print("Updating Model")
+        updater.update_model(calc_grad=True)
 
-    # Track Stats
-    updater.print_stats()
+        # Track Stats
+        updater.print_stats()
 
 print("All Done")
